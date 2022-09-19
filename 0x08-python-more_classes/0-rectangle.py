c9c8664f9ec6d17fdafb0d5c13949f613a1fb59a
97Ban <moetibanele5@gmail.com>
2022-09-19 17:01:54 +0200

    My python

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,11 +1,7 @@
-
 #!/usr/bin/python3
-"""Module 0-rectangle
-Defines an empty Rectangle class
-"""
+""" Rectangle Module"""
 
 
 class Rectangle:
-    """Empty class"""
+    """Empty Rectangle Module"""
     pass
-Footer
